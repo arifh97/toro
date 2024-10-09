b9aaaa385efdfe2f30b80d1df14d0e546b1ffcad
Hassan Ali <hassan4np@gmail.com>
2024-10-10 02:56:40 +0600

    upd

--- src/components/Galary2.jsx
+++ src/components/Galary2.jsx
@@ -7,41 +7,61 @@ import { BsPlayCircleFill } from "react-icons/bs";
 
 export default function Galary() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null); 
+  const [showButton, setShowButton] = useState(true);
+  const videoRef = useRef(null);
 
-  const handleVideoPlay = () => {
+  const handleVideoPlayPause = () => {
     if (isPlaying) {
-      videoRef.current.pause(); 
+      videoRef.current.pause();
+      setShowButton(true);
     } else {
       videoRef.current.play();
+      setShowButton(false);
     }
     setIsPlaying(!isPlaying);
+  };
+
+  const handleMouseEnter = () => {
+    if (isPlaying) {
+      setShowButton(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (isPlaying) {
+      setShowButton(false);
+    }
   };
 
   return (
     <div className='galary2'>
       <Container>
-        <Row> 
+        <Row>
           <Col xs={12} md={5} lg={4} className='mb-5 md:mb-0'>
-            <div className="galary2-left w-full  border-white border-[2px] lg:border-[3] xl:border-[5px] object-cover"> 
+            <div className="galary2-left w-full border-white border-[2px] lg:border-[3] xl:border-[5px] object-cover">
               <img className='w-full h-full object-cover' src={Img} alt="Gallery" />
             </div>
           </Col>
-
           <Col xs={12} md={7} lg={8}>
-            <div className="relative z-[1] galary2-right w-full border-white border-[2px] lg:border-[3] xl:border-[5px]">
+            <div
+              className="relative z-[1] galary2-right w-full border-white border-[2px] lg:border-[3] xl:border-[5px]"
+              onMouseEnter={handleMouseEnter} 
+              onMouseLeave={handleMouseLeave}
+            >
               <video ref={videoRef} loop src={videos} className="w-full h-full object-cover"></video>
-              <button
-                onClick={handleVideoPlay}
-                className="absolute  border-0 p-0 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] bg-transparent z-10 text-white rounded"
-              >
-                {isPlaying ? <FaRegCirclePause /> : <BsPlayCircleFill />}
-              </button>
+              {showButton && (
+                <button
+                  onClick={handleVideoPlayPause}
+                  className="absolute border-0 p-0 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] bg-transparent z-10 text-white rounded"
+                >
+                  {isPlaying ? <FaRegCirclePause /> : <BsPlayCircleFill />}
+                </button>
+              )}
             </div>
           </Col>
 
         </Row>
       </Container>
     </div>
-  )
+  );
 }
