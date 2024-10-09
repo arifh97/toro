db39948495df7d2094bf4a54b20e2b529565bb6b
mehedi3365 <mehedihassanemon369@gmail.com>
2024-10-10 00:53:11 +0600

    working

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from './components/Header'
 import Banner from './components/Banner'
 import About from './components/About'
 import Footer from './components/Footer'
+import Meet from './components/Meet'
 
 export default function App() {
   return (
     <>
       <Header />
+      <Meet/>
       <Banner />
       <About />
       <Footer />
---
+++ src/components/Meet.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "./Grid"
+
+export default function Meet() {
+  return (
+    <div className='meet'>
+      <Container>
+        <Row>
+            <Col>
+            
+            </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
