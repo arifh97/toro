8e8d8449c7228b8ad23e7d2d5c4372592d71f3fa
Hassan Ali <hassan4np@gmail.com>
2024-10-10 01:16:31 +0600

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from './components/Header'
 import Banner from './components/Banner'
 import About from './components/About'
 import Footer from './components/Footer'
+import Galary from './components/Galary'
+import Galary2 from './components/Galary2'
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Header />
       <Banner />
       <About />
+      <Galary/>
+      <Galary2/>
       <Footer />
       
     </>
---
+++ src/components/Galary.jsx
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from './Grid'
+import Img from "../assets/img/galary1.png"
+import Img2 from "../assets/img/galary2.png"
+
+export default function Galary() {
+  return (
+    <div className='galary mb-8 lg:mb-10'>
+        <Container>
+            <Row>
+                <Col xs={12} md={6} lg={8}>
+                    <div className="galary-left w-full  border-white border-[2px] lg:border-[3] xl:border-[5px] "> <img  className='w-full h-full' src={Img} alt="" /></div>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                <div className="galary-right w-full  border-white border-[2px] lg:border-[3] xl:border-[5px] "> <img  className='w-full h-full' src={Img2} alt="" /></div>
+                </Col>
+            </Row>
+        </Container>
+    </div>
+  )
+}
